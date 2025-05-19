3fcea2986d244c17f1108e95aba7f0f8311b030f
Kent Yeh <kent.yeh2000@gmail.com>
2025-05-19 09:16:02 +0800

    改佈局為spring managed beans

---
+++ src/main/java/com/example/demotroad/AppConfig.java
@@ -0,0 +1,62 @@
+package com.example.demotroad;
+
+import java.security.GeneralSecurityException;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Kent Yeh
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean(destroyMethod = "close")
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @Lazy
+    public HttpClientConnectionManager httpClientConnectionManager() throws GeneralSecurityException {
+        return PoolingHttpClientConnectionManagerBuilder.create()
+                .setTlsSocketStrategy(
+                        new DefaultClientTlsStrategy(
+                                SSLContexts.custom()
+                                        .loadTrustMaterial(null, (chain, authType) -> true)
+                                        .build(),
+                                (host, session) -> true))
+                .build();
+    }
+
+    @Bean(destroyMethod = "close")
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @Lazy
+    public HttpClient httpClient() throws GeneralSecurityException {
+        return HttpClients.custom().setConnectionManager(httpClientConnectionManager()).build();
+    }
+
+    @Bean(destroyMethod = "destroy")
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @Lazy
+    public ClientHttpRequestFactory clientHttpRequestFactory() throws GeneralSecurityException {
+        HttpComponentsClientHttpRequestFactory hcchrf = new HttpComponentsClientHttpRequestFactory();
+        hcchrf.setHttpClient(httpClient());
+        return hcchrf;
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    @Lazy
+    public RestTemplate restTemplate() throws GeneralSecurityException {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+}
